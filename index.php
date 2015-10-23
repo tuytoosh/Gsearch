acd9273f167a076554c663737cb1df5cd6e1cb29
Hamid Haghdoost <tuytoosh@yahoo.com>
2015-10-24 01:28:13 +0330

    Create index.php
    index page

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+        $keyword = urlencode($str);
+        $url     = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=$keyword&userip=192.168.106.1";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_REFERER, url('/') );
+        $body = curl_exec($ch);
+        curl_close($ch);
+        $json = json_decode($body);
