a5a14adea66cd2860e2b8b679c344e4d3ae385ed
Hamid Haghdoost <tuytoosh@yahoo.com>
2015-10-24 01:36:00 +0330

    Update index.php
    Gsearch class created with search static method

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
-        $keyword = urlencode($str);
-        $url     = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=$keyword&userip=192.168.106.1";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_REFERER, url('/') );
-        $body = curl_exec($ch);
-        curl_close($ch);
-        $json = json_decode($body);
+namespace tuytoosh/php-google-web-search;
+        class Gsearch
+        {
+                public static function Search($str)
+                {
+                        $keyword = urlencode($str);
+                        $url     = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=$keyword&userip=192.168.106.1";
+                        $ch = curl_init();
+                        curl_setopt($ch, CURLOPT_URL, $url);
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                        curl_setopt($ch, CURLOPT_REFERER, url('/') );
+                        $body = curl_exec($ch);
+                        curl_close($ch);
+                        $json = json_decode($body);
+                        return $json;
+                }
+        }
