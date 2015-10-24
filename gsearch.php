b0e2d8b2f491d3fb252ed97b1af2239cbd10b33c
Hamid Haghdoost <tuytoosh@yahoo.com>
2015-10-24 20:18:35 +0330

    Update and rename Gsearch.php to gsearch.php

--- gsearch.php
+++ gsearch.php
@@ -1,16 +1,17 @@
 <?php namespace tuytoosh/php-google-web-search;
 
+use tuytoosh/gsearch;
 
-        class Gsearch
+        class gsearch
         {
-                public static function Search($str)
+                public static function search($str)
                 {
                         $keyword = urlencode($str);
                         $url     = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=$keyword&userip=192.168.106.1";
                         $ch = curl_init();
                         curl_setopt($ch, CURLOPT_URL, $url);
                         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                        curl_setopt($ch, CURLOPT_REFERER, url('/') );
+                        curl_setopt($ch, CURLOPT_REFERER, config::get('website_url') );
                         $body = curl_exec($ch);
                         curl_close($ch);
                         $json = json_decode($body);
